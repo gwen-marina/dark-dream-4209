de8af15e7ff3febc88c11b1359e992b9853af25b
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-07-19 16:36:18 -0700

    User story 4 completed

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -23,4 +23,25 @@ RSpec.describe 'recipes show page' do
     expect(page).to have_content('Butter')
     expect(page).to_not have_content('Meat')
   end
+
+  it 'can show the total cost of a recipe' do
+    cheeseburger = Recipe.create!(name: "Cheese Burger", complexity: 2, genre: "American")
+    spaghetti = Recipe.create!(name: "Spaghetti", complexity: 3, genre: "Italian")
+
+    cheese = Ingredient.create!(name: "American Cheese", cost: 4)
+    bread = Ingredient.create!(name: "Bread", cost: 3)
+    pasta = Ingredient.create!(name: "Pasta", cost: 2)
+    meat = Ingredient.create!(name: "Meat", cost: 7)
+
+    recipe_ingredient_1 = RecipeIngredient.create!(recipe_id: cheeseburger.id, ingredient_id: cheese.id)
+    recipe_ingredient_2 = RecipeIngredient.create!(recipe_id: cheeseburger.id, ingredient_id: bread.id)
+    recipe_ingredient_3 = RecipeIngredient.create!(recipe_id: cheeseburger.id, ingredient_id: meat.id)
+    recipe_ingredient_4 = RecipeIngredient.create!(recipe_id: spaghetti.id, ingredient_id: meat.id)
+    recipe_ingredient_5 = RecipeIngredient.create!(recipe_id: spaghetti.id, ingredient_id: pasta.id)
+
+    visit "/recipes/#{cheeseburger.id}"
+
+    expect(page).to have_content("Total Cost: 14")
+    expect(page).to_not have_content("Total Cost: 9")
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,26 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe 'model methods' do 
+    it '#total_cost'do 
+      cheeseburger = Recipe.create!(name: "Cheese Burger", complexity: 2, genre: "American")
+      spaghetti = Recipe.create!(name: "Spaghetti", complexity: 3, genre: "Italian")
+
+      cheese = Ingredient.create!(name: "American Cheese", cost: 4)
+      bread = Ingredient.create!(name: "Bread", cost: 3)
+      pasta = Ingredient.create!(name: "Pasta", cost: 2)
+      meat = Ingredient.create!(name: "Meat", cost: 7)
+
+      recipe_ingredient_1 = RecipeIngredient.create!(recipe_id: cheeseburger.id, ingredient_id: cheese.id)
+      recipe_ingredient_2 = RecipeIngredient.create!(recipe_id: cheeseburger.id, ingredient_id: bread.id)
+      recipe_ingredient_3 = RecipeIngredient.create!(recipe_id: cheeseburger.id, ingredient_id: meat.id)
+      recipe_ingredient_4 = RecipeIngredient.create!(recipe_id: spaghetti.id, ingredient_id: meat.id)
+      recipe_ingredient_5 = RecipeIngredient.create!(recipe_id: spaghetti.id, ingredient_id: pasta.id)
+
+      expect(cheeseburger.total_cost).to eq(14)
+      expect(spaghetti.total_cost).to eq(9)
+
+    end
+  end
 end
